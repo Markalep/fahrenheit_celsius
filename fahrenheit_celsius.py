9eca1180aebeb4647ef4f5de57eed3b953495eb3
Markalep <armand.oliveira.shinai@hotmail.com>
2022-10-26 17:14:16 -0300

    Tela de conversão

---
+++ fahrenheit_celsius.py
@@ -0,0 +1,39 @@
+from cProfile import label
+from tkinter import *
+
+#°C = (°F - 32) ÷ 1,8
+
+#-------------------------------------------------------------
+# GUI
+
+root = Tk()
+root.title('App')
+
+final = StringVar()
+
+#-------------------------------------------------------------
+# funções
+
+def calcular():
+    F = float(textbox1.get())
+    C = (F - 32)* 5/9
+    final.set(str(round(C, 1)) + ' graus Celsius')
+
+#-------------------------------------------------------------
+# widgets
+
+label1 = Label(root, text = 'Fahrenheit:')
+textbox1 = Entry(root)
+btn = Button(root, text = 'Calcular:', command=calcular)
+label_resultado = Label(root, textvariable=final)
+
+#-------------------------------------------------------------
+# layout
+
+label1.grid()
+textbox1.grid()
+btn.grid()
+label_resultado.grid()
+
+
+root.mainloop()
